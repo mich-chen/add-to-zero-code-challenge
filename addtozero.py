482f2f5b50ad56fbc0317f87e6d934071e43da1e
Michelle Chen <mich.chen.94@gmail.com>
2020-07-20 18:11:31 -0700

    completed add_to_zero challenge by finding a number and its negative

--- addtozero.py
+++ addtozero.py
@@ -20,9 +20,25 @@ zero for both numbers, and they sum to zero:
     True
 """
 
+# maybe can use itertools.combinations_with_replacement(iterable, r)
+# finding combinations with every possible permutation, including adding to itself
 
 def add_to_zero(nums):
     """Given list of ints, return True if any two nums sum to 0."""
+
+    # set_nums = set(nums)
+    # if 0 in set_nums:
+    #     return True
+    # do not need the above block of code because python considers -0 == 0)
+
+    set_nums = set(nums)
+    for num in set_nums:
+        if -num in set_nums:
+            return True
+
+    else:
+        return False
+    # for num in set_nums
 
 
 if __name__ == '__main__':
